b1e520a3176e9f611a7f2bd71aaae207b9f7b4dd
scottwegley <scottwegley@gmail.com>
2022-12-21 15:44:24 -0800

    altering setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+const client = new discord_js_1.Client({ intents: ['Guilds', 'GuildMessages'] });
+client.login();
