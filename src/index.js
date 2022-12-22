7be91889995b27e1e13b434cc9e6f12b54d29cc4
scottwegley <scottwegley@gmail.com>
2022-12-21 16:21:52 -0800

    basic tutorial bot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const discord_js_1 = require("discord.js");
-const client = new discord_js_1.Client({ intents: ['Guilds', 'GuildMessages'] });
-client.login();
+import { config } from 'dotenv';
+import { Client, CommandInteractionOptionResolver, GatewayIntentBits, Routes } from 'discord.js';
+import { REST } from '@discordjs/rest';
+
+config();
+const TOKEN = process.env.SADI_TOKEN;
+const CLIENT_ID = process.env.SADI_CLIENT_ID
+const GUILD_ID = process.env.THELAB_ID;
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+const rest = new REST({ version: '10' }).setToken(TOKEN)
+
+client.on('ready', () => { console.log(`${client.user.tag} has logged in.`) });
+
+async function main() {
+    const commmands = [
+        {
+            name: 'ping',
+            description: 'Replies with "Pong!"',
+        },
+    ];
+
+    try {
+        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+            body: commmands,
+        });
+        client.login(TOKEN);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main();
