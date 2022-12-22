2b00d5bae356fc5dab42ed33e2cd2e11176f6f8c
scottwegley <scottwegley@gmail.com>
2022-12-22 14:51:45 -0800

    updated ready event to use success instead of log

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,8 @@
-import { makeLog } from "../utils/ColorfulConsole.js";
+import { makeSuccess } from "../utils/ColorfulConsole.js";
 import { CustomClient } from "../utils/CustomClient.js";
 
 export const name = 'ready';
 export const once = true;
 export function execute(client:CustomClient){
-    makeLog(`${client.user?.tag} has logged in.`);
+    makeSuccess(`${client.user?.tag} has logged in.`);
 }
