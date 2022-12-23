2be383d62764550cf990e49ed60da4e9040710ed
scottwegley <scottwegley@gmail.com>
2022-12-23 01:00:46 -0800

    documentation

--- bin/utils/CustomClient.js
+++ bin/utils/CustomClient.js
@@ -1,4 +1,7 @@
 import { Client } from "discord.js";
+/**
+ * Intermediary type only different from `Client` by a `Collection` to store command json and the command object itself.
+ */
 class CustomClient extends Client {
     constructor(_options, _commands) {
         super(_options);
--- src/utils/CustomClient.ts
+++ src/utils/CustomClient.ts
@@ -1,5 +1,8 @@
 import { Client, ClientOptions, Collection } from "discord.js";
 
+/**
+ * Intermediary type only different from `Client` by a `Collection` to store command json and the command object itself.
+ */
 class CustomClient extends Client {
     commands: Collection<any, any>;
     constructor(_options: ClientOptions, _commands: Collection<any, any>) {
