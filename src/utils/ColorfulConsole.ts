a35f880e09e926712c12108070a2409c4f2fdbfc
scottwegley <scottwegley@gmail.com>
2022-12-23 00:33:18 -0800

    updated internal log function to take in modifiers seperates handling of colors and message types

--- src/utils/ColorfulConsole.ts
+++ src/utils/ColorfulConsole.ts
@@ -16,12 +16,12 @@ function makeHeading(msg:string) {
     customLog(`${ConsoleCodes.fg.Blue}[HEADING] ${msg}`)
 }
 
-function makeSuccess(msg:string) {
-    customLog(`${ConsoleCodes.fg.Green}[SUCCESS] ${msg}`)
+function customLog(msg: string, mods: (Foreground | Background | Effects | MessageType)[]) {
 }
 
-function customLog(msg:string){
-    console.log(`%s${ConsoleCodes.effects.Reset}`,`${msg}`)
+function timestamp() {
+    let today = new Date();
+    return `${today.getHours().toLocaleString('en-US', { minimumIntegerDigits: 2 })}:${today.getMinutes().toLocaleString('en-US', { minimumIntegerDigits: 2 })}:${today.getSeconds().toLocaleString('en-US', { minimumIntegerDigits: 2 })}`;
 }
 
 export { makeError, makeWarning, makeLog, makeHeading, makeSuccess };
