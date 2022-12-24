c17198a4cb772b0a01d5315753672c66d3a32d0b
scottwegley <scottwegley@gmail.com>
2022-12-23 21:13:22 -0800

    added option for number of msgs to delete

--- src/commands/delete.ts
+++ src/commands/delete.ts
@@ -2,7 +2,13 @@ import { CommandInteraction, makeError, SlashCommandBuilder, TextChannel } from 
 
 export const data = new SlashCommandBuilder()
     .setName('delete')
-    .setDescription('Deletes X number of messages');
+    .setDescription('Deletes X number of messages')
+    .addIntegerOption((option) =>
+        option.setName('amount')
+            .setDescription('The number os messages to delete (default 100)')
+            .setMinValue(1)
+            .setMaxValue(100)
+    );
 
 export async function execute(interaction: CommandInteraction) {
     await interaction.reply(`Deleting X Messages!`);
