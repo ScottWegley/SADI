13c40415a6d2e95a49321f76822cf30d06b56620
scottwegley <scottwegley@gmail.com>
2022-12-23 21:16:06 -0800

    compilation

--- bin/commands/delete.js
+++ bin/commands/delete.js
@@ -1,11 +1,18 @@
 import { SlashCommandBuilder } from "discord.js";
+import { makeLog } from "../utils/ColorfulConsole.js";
 export const data = new SlashCommandBuilder()
     .setName('delete')
-    .setDescription('Deletes X number of messages');
+    .setDescription('Deletes X number of messages')
+    .addIntegerOption((option) => option.setName('amount')
+    .setDescription('The number os messages to delete (default and max 100)')
+    .setMinValue(1)
+    .setMaxValue(100));
 export async function execute(interaction) {
-    await interaction.reply(`Deleting X Messages!`);
+    let amt = interaction.options.getInteger('amount') || 100;
+    await interaction.reply({ content: `Deleting ${amt} Messages!`, ephemeral: true });
     try {
-        interaction.channel.bulkDelete(100);
+        interaction.channel.bulkDelete(amt);
+        makeLog(`Deleted ${amt} messsages`);
     }
     catch (error) {
         console.log(error);
